7c49c666371b134c48ac7c76c4c043bcf4ca6db5
Stef432 <stefaniaurriza@gmail.com>
2024-03-21 16:18:22 +0100

    implementing useRef

--- client/src/Components/ContactUs.jsx
+++ client/src/Components/ContactUs.jsx
@@ -3,14 +3,18 @@ import emailjs from "@emailjs/browser";
 import { getParsedCommandLineOfConfigFile, setEmitFlags } from "typescript";
 
 export const ContactUs = () => {
+  const userNameRef = useRef();
+  const userEmailRef = useRef();
+  const messageRef = useRef();
+
   function handleSubmit(e) {
     e.preventDefault();
     const serviceId = "service_hmy3li6";
     const templateId = "template_5g0sfqe";
     const object = {
-      user_name: FormData.get("user_name"),
-      user_email: FormData.get("user_email"),
-      message: FormData.get("message"),
+      user_name: userNameRef.current.value,
+      user_email: userEmailRef.current.value,
+      message: messageRef.current.value,
     };
 
     console.log(object);
@@ -28,18 +32,27 @@ export const ContactUs = () => {
           console.log("FAILED...", error);
         }
       );
+
+    userNameRef.current.value = "";
+    userEmailRef.current.value = "";
+    messageRef.current.value = "";
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="user_name">Name</label>
-      <input type="text" name="user_name" id="user_name" />
+      <input type="text" name="user_name" id="user_name" ref={userNameRef} />
 
       <label htmlFor="user_email">Email</label>
-      <input type="email" name="user_email" id="user_email" />
+      <input
+        type="email"
+        name="user_email"
+        id="user_email"
+        ref={userEmailRef}
+      />
 
       <label htmlFor="message">Message</label>
-      <input name="message" id="message" />
+      <input name="message" id="message" ref={messageRef} />
 
       <button type="submit" />
     </form>
